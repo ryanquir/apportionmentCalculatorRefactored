5897b65b8a34da549a3e34ff0f539e382211f99c
Pranie63 <Pranav Ramkumar>
2023-03-18 16:40:53 -0400

    Implemented total population method in relative benefit format class

--- src/main/java/edu/virginia/cs/hw3/RelativeBenefitFormat.java
+++ src/main/java/edu/virginia/cs/hw3/RelativeBenefitFormat.java
@@ -1,4 +1,35 @@
 package edu.virginia.cs.hw3;
 
-public class RelativeBenefitFormat {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class RelativeBenefitFormat extends ApportionmentFormat{
+
+    Map<State, List<Integer>> relativeBenefitData = new HashMap<>();
+    @Override
+    public String getApportionmentString(Apportionment apportionment) {
+        int totalPopulation = getTotalPopulation(apportionment);
+        return null;
+    }
+
+    private int getTotalPopulation(Apportionment apportionment) {
+        Set<State> stateSet = apportionment.apportionmentMap.keySet();
+        int totalPopulation = 0;
+        for (State state : stateSet) {
+            totalPopulation += state.getPopulation();
+        }
+        return totalPopulation;
+    }
+
+    private double getDivisor(Apportionment apportionment) {
+        int totalPopulation = getTotalPopulation(apportionment);
+        return (double) totalPopulation / apportionment.getAllocatedRepresentatives();
+    }
+
+    private void storeData(Apportionment apportionment) {
+
+    }
+
 }
