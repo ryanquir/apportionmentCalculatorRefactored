dad5cfcb0f361f58022e9540dae21ae3fb4e0e9f
ryanquir <zhe5qq@virginia.edu>
2023-03-18 16:23:20 -0400

    ApportionmentFormatFactory complete - RelativeBenefitFormat needs to be written

--- src/main/java/edu/virginia/cs/hw3/ApportionmentFormatFactory.java
+++ src/main/java/edu/virginia/cs/hw3/ApportionmentFormatFactory.java
@@ -2,6 +2,13 @@ package edu.virginia.cs.hw3;
 
 public class ApportionmentFormatFactory {
     ApportionmentFormat getFormat(String formatName) {
-
+        if (formatName.equalsIgnoreCase("alpha")) {
+            return new AlphabeticalApportionmentFormat();
+        } else if (formatName.equalsIgnoreCase("benefit")) {
+            return new RelativeBenefitFormat();
+        } else {
+            throw new IllegalArgumentException("Error: invalid format type. The system currently supports:\n" +
+                    "\talpha, benefit");
+        }
     }
 }
