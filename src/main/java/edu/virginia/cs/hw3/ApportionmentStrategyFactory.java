fb4409f583232e97985a7a972e7d7798ce9ac2bd
ryanquir <zhe5qq@virginia.edu>
2023-03-18 16:15:30 -0400

    ApportionmentStrategyFactory complete

--- src/main/java/edu/virginia/cs/hw3/ApportionmentStrategyFactory.java
+++ src/main/java/edu/virginia/cs/hw3/ApportionmentStrategyFactory.java
@@ -2,6 +2,15 @@ package edu.virginia.cs.hw3;
 
 public class ApportionmentStrategyFactory {
     ApportionmentStrategy getStrategy(String algorithmName) {
-
+        if (algorithmName.equalsIgnoreCase("hamilton")) {
+            return new HamiltonApportionmentStrategy();
+        } else if (algorithmName.equalsIgnoreCase("huntington")) {
+            return new HuntingtonHillApportionmentStrategy();
+        } else if (algorithmName.equalsIgnoreCase("jefferson")) {
+            return new JeffersonApportionmentStrategy();
+        } else {
+            throw new IllegalArgumentException("Error: invalid algorithm type. The system currently supports:\n" +
+                    "\thamilton, huntington, jefferson");
+        }
     }
 }
